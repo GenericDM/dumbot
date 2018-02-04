c1b67f6b2e6e29d19af1f881688526acfd640533
GenericDM <34109002+GenericDM@users.noreply.github.com>
2018-02-04 09:46:38 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,33 @@
+const fs = require("fs");
 const Discord = require("discord.js");
-
-const TOKEN = "your bot here";
-const PREFIX = "1";
-
 var bot = new Discord.Client();
-
-
-bot.login(TOKEN);
-
+const auth = require("./auth.json");
+//made from CopperBanana's weird bot script, thanks Copper!
+bot.login(auth.token);
+var prefix = auth.prefix
 bot.on("ready", function () {
-    console.log("u can do stufz now");
-	bot.user.setGame('with the gay');
+    console.log("lol hi im alive");
+	bot.user.setActivity('prefix is: ' + (prefix));
 	bot.user.setStatus('dnd');
 });
 
 bot.on("message", function (message) {
 
-    if (!message.content.startsWith(PREFIX)) return;
+    if (!message.content.startsWith(auth.prefix)) return;
 
-    var args = message.content.substring(PREFIX.length).split(" ");
+    var args = message.content.substring((auth.prefix).length).split(" ");
 
     switch (args[0].toLowerCase()) {
+    case 'changeprefix':
+      let newPrefix = message.content.split(" ").slice(1,2)[0];
+      auth.prefix = newPrefix;
+      var prefix = newPrefix;
+
+      fs.writeFile("./auth.json", JSON.stringify(auth), (err) => console.error);
+      break;
 		case 'number':
 			var randomNumber = Math.floor(Math.random() * 99999999999999999) + 1;
-			message.channel.send('Here is ur random number: ' + randomNumber);
-			break;
-		case 'announcement':
-			message.guild.channels.find("name", "announcements").send("@everyone 1")
+			message.channel.send('wowee that sure is randem xd: ' + randomNumber)
 			break;
 		case 'lmao':
 			var embed = new Discord.RichEmbed()
@@ -39,46 +40,26 @@ bot.on("message", function (message) {
 				.setColor('RANDOM')
 			message.channel.sendEmbed(embed)
 			break;
-		case 'pizza':
-			message.channel.send('🍕')
-			message.channel.send('<:GWjiangoOmegaLUL:389904150886088723>')
-			break;
 		case 'shit':
 			var embed = new Discord.RichEmbed()
 				.setImage('https://www.youtube.com/watch?v=rouKUSoYjVw')
 				.setColor('RANDOM')
 			message.channel.sendEmbed(embed)
 			break;
-		case 'forcedannouncement':
-			message.guild.channels.find('name', 'announcements').send('<@&356550232441356290> 1')
-			message.channel.send('<:lmao:356556240609869824>')
-			break;
 		case 'idiot':
 			message.member.setNickname('dumbass')
 			break;
-		case 'dab':
-			message.channel.send('<:GWsquidsWhiteboy1:407619013183668245><:GWsquidsWhiteboy2:407619019483381771><:GWsquidsWhiteboy3:407619026009849857>')
-			break;
-		case 'giveadmin':
-			message.author.send('https://discord.gg/E9N7wc5')
-			message.member.kick()
-			break;
 		case 'help':
 			message.channel.send('Commands sent to you in your DMs')
-			message.author.send('- 1help - Boi you should know what this command does')
-			message.author.send('- 1vote - Creates a vote in the reactions on your message')
-			message.author.send('- 1everyone - Pings everyone.')
-			message.author.send('- 1xd - xd')
-			message.author.send('- 1yeah - yeah')
-			message.author.send('- 1say - Says whatever you say.')
-			message.author.send('- 1kickme - Kicks you xd')
-			message.author.send('- 1giveadmin - Gives you admin.')
-			message.author.send('- 1randomnumber - I was bored okay?')
-			message.author.send('- 1announcement - Pings @everyone with 1. Only works in <@344960470140321794>s server (unless the server in question has a channel named #announcements exactly and the bot has perms too).')
-			message.author.send('- 1forcedannouncement - Same as command above but it forces a ping and REALLY ONLY WORKS IN <@344960470140321794>s server.')
-			message.author.send('- 1idiot - fuckin dumbass')
-			message.author.send('- 1lmao - :lmao:')
-			message.author.send('- 1one - 1 gif')
+			message.author.send('- help - take a guess')
+			message.author.send('- vote - makes ur message into a vote')
+			message.author.send('- xd - xd')
+			message.author.send('- yeah - yeah')
+      message.author.send("-everyone - @'s everyone")
+			message.author.send('- say - Says whatever you say.')
+			message.author.send('- number - makes a random numbr')
+			message.author.send('- idiot - fuckin dumbass')
+			message.author.send('- one - 1 gif')
 			break;
 		case 'vote':
 			message.react('👍')
@@ -90,7 +71,8 @@ bot.on("message", function (message) {
 			message.react('📌')
 			break;
 		case "everyone":
-			message.channel.send('@everyone')
+			message.channel.send(' ' + message.author)
+			message.channel.send('Get fucked')
             break;
 		case "xd":
 			message.channel.send('xd')
@@ -99,11 +81,14 @@ bot.on("message", function (message) {
 			message.channel.send('yeah')
 			break;
 		case 'say':
-			let text = message.content.slice(PREFIX.length).trim().split(/ +/g).slice(1).join(" ");
+			let text = message.content.slice((auth.prefix).length).trim().split(/ +/g).slice(1).join(" ");
 			message.channel.send(text, {
 				tts: false
 			});
 			console.log(text);
+			break;
+		case 'gay':
+			message.channel.send("no u")
 			break;
 		case 'pants':
 			message.channel.send('👖👖👖👖👖👖👖')
@@ -111,17 +96,18 @@ bot.on("message", function (message) {
 			message.channel.send('👖👖👖👖👖👖👖')
 			message.channel.send('👖👖👖👖👖👖👖')
 			message.channel.send('👖👖👖👖👖👖👖')
+      break;
         default:
 		    if (message.author.equals(bot.user)) return;
-			message.channel.send("1", {
+			message.channel.send("I don't recognise this command, try something else!", {
 				tts: true
 			})
-			message.react('1⃣')
+			message.react('🇪')
     }
 });
 
 bot.on("message", function (message) {
-	
+
 	if (message.content === '@everyone') {
 	message.channel.send('Pinged lmao')
 	}
